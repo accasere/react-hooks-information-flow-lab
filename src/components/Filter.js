0ae6cf856535fd8d62add7ae05dbb50d502966ba
Antony Caseres <accasere@outlook.com>
2022-06-18 22:34:40 -0500

    Completed Assigment

---
+++ src/components/Filter.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function Filter(props) {
+    return(
+        <div>
+            <div className="Filter">
+                <select name="filter" onChange={props.onCategoryChange}>
+                    <option value="All">Filter by category</option>
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Dessert">Dessert</option>
+
+                </select>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Filter;
+
+/*import React from 'react';
+
+function Filter(props) {
+
+    return (<div>       
+          <div className="Filter">
+            <select name="filter" onChange={props.handleCategoryChange}>
+              <option value="All">Filter by category</option>
+              <option value="Produce">Produce</option>
+              <option value="Dairy">Dairy</option>
+              <option value="Dessert">Dessert</option>
+            </select>
+          </div>
+        </div>
+        
+      );
+
+}
+export default Filter; */
